75eb15961aab1c3bae6d3f1bce6dfd9453f28ee2
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-05-21 13:00:44 +0530

    change id type

--- models/course.go
+++ models/course.go
@@ -1,15 +1,17 @@
 package models
 
-import "go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Course struct {
-	UUID    uuid.UUID `json:"uuid" bson:"uuid"`
-	Code    string    `json:"code" bson:"code"`
-	Credits int       `json:"credits" bson:"credits"`
-	Faculty string    `json:"faculty" bson:"faculty"`
-	Owner   string    `json:"owner" bson:"owner"`
-	Room    string    `json:"room" bson:"room"`
-	Slot    string    `json:"slot" bson:"slot"`
-	Title   string    `json:"title" bson:"title"`
-	Type    string    `json:"type" bson:"type"`
+	ID      primitive.ObjectID `json:"_id" bson:"_id"`
+	Code    string             `json:"code" bson:"code"`
+	Credits int                `json:"credits" bson:"credits"`
+	Faculty string             `json:"faculty" bson:"faculty"`
+	Owner   string             `json:"owner" bson:"owner"`
+	Room    string             `json:"room" bson:"room"`
+	Slot    string             `json:"slot" bson:"slot"`
+	Title   string             `json:"title" bson:"title"`
+	Type    string             `json:"type" bson:"type"`
 }

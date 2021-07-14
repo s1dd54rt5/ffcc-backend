3550df11c41524a1c7c4dd469e86480a25dccb06
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-07-14 21:33:56 +0530

    cors fix

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.1
+	github.com/itsjamie/gin-cors v0.0.0-20160420130702-97b4a9da7933 // indirect
 	github.com/joho/godotenv v1.3.0
 	go.mongodb.org/mongo-driver v1.5.2
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/44t4nk1/ffcc-backend/db"
-	"github.com/44t4nk1/ffcc-backend/middlewares"
 	"github.com/44t4nk1/ffcc-backend/routes"
 	"github.com/gin-gonic/gin"
+	cors "github.com/itsjamie/gin-cors"
 )
 
 var (
@@ -24,6 +25,15 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	router.Use(middlewares.CORSMiddleware())
+	router.Use(cors.Middleware(
+		cors.Config{
+			Origins:         "*",
+			Methods:         "GET, PUT, POST, DELETE",
+			RequestHeaders:  "Origin, Authorization, Content-Type",
+			ExposedHeaders:  "",
+			MaxAge:          50 * time.Second,
+			Credentials:     true,
+			ValidateHeaders: false,
+		}))
 	log.Fatal(router.Run(":" + port))
 }
